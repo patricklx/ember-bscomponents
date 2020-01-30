28a0fd0c7917e41ad2027bd9e58dc4ace5cfe312
patrick <patrick.pircher@ibm.com>
2020-01-30 09:09:04 +0100

    to es6

---
+++ addon/components/bs-alert/component.js
@@ -0,0 +1,39 @@
+import Component from '@ember/component';
+import { later } from '@ember/runloop';
+import { action } from '@ember/object';
+import layout from './template';
+
+class BsAlertComponent extends Component {
+  layout = layout;
+  dismissAfter = 0;
+  dismissible = true;
+  isVisible = true;
+  type = 'info';
+
+  didInsertElement() {
+    var autoDismiss;
+    if (this.dismissAfter > 0) {
+      autoDismiss = function() {
+        let base;
+        if (this.isDestroyed) {
+          return;
+        }
+        if (typeof (base = this.attrs).onAutoDismiss === "function") {
+          base.onAutoDismiss();
+        }
+        this.set('isVisible', false);
+      };
+      later(this, autoDismiss, this.dismissAfter * 1000);
+    }
+  }
+
+  @action
+  dismiss() {
+    const p = typeof this.attrs.onDismiss === "function" && this.attrs.onDismiss();
+    return Ember.RSVP.resolve(p).finally(() => {
+      this.set('isVisible', false);
+    });
+  }
+}
+
+export default BsAlertComponent;
