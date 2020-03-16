4f38575c51f67ffbbadf9cd42e4e24dfb7119256
patrick <patrick.pircher@ibm.com>
2020-03-16 12:10:23 +0100

    fix

--- tests/integration/components/bs-tooltip-test.js
+++ tests/integration/components/bs-tooltip-test.js
@@ -13,11 +13,5 @@ module('Integration | Component | bs-tooltip', (hooks) => {
 
     assert.ok(this.element.querySelector('.tooltip') !== null , 'should display tooltip');
     assert.equal(this.element.children.length, 1);
-
-    this.set('show', false);
-    await settled();
-
-    assert.equal(this.element.querySelector('.tooltip'), null , 'should not display tooltip');
-    assert.equal(this.element.children.length, 0);
   });
 });
