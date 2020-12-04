f7ab5ef4dd3e3443015755fb9bc6693eeda350ae
patrick <patrick.pircher@ibm.com>
2020-12-04 15:30:18 +0100

    use ember import

--- addon/components/bs-button/component.js
+++ addon/components/bs-button/component.js
@@ -1,16 +1,8 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import layout from './template';
 
 class BsButtonComponent extends Component {
   layout = layout;
-
-  get args() {
-    const o = {};
-    Object.keys(this.attrs).forEach((k) => {
-      o[k] = this.get(k);
-    });
-    return o;
-  }
 }
 
 export default BsButtonComponent;
--- addon/components/bs-pagination/component.js
+++ addon/components/bs-pagination/component.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import template from './template';
 import { tracked } from "@glimmer/tracking";
 
