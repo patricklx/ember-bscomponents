6ae5274aee46c08249086e135d51369280902a94
patrick <patrick.pircher@ibm.com>
2021-05-31 14:21:02 +0200

    auto disable button during promise

--- addon/components/bs-button/component.ts
+++ addon/components/bs-button/component.ts
@@ -1,9 +1,26 @@
 import Component from '@glimmer/component';
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
 type Args = {
   type: 'primary'|'secondary'|'default'|'warning';
   size: 'sm'|'lg'|'md'|'xs';
+  onClick(): Promise<any>|null
 }
 
 // eslint-disable-next-line ember/no-empty-glimmer-component-classes
-export default class BsButtonComponent extends Component<Args> {}
+export default class BsButtonComponent extends Component<Args> {
+
+  @tracked isLoading = false;
+
+  @action
+  onClick() {
+    const p = this.args.onClick();
+    if (p && p.then) {
+      this.isLoading = true;
+      p.finally(() => {
+        this.isLoading = false;
+      })
+    }
+  }
+}
