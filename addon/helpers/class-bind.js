a7625971548ceadbe59bbb519507f2fc2ee32bb5
patricklx <patricklx@users.noreply.github.com>
2022-05-19 14:32:00 +0200

    Update class-bind.js

--- addon/helpers/class-bind.js
+++ addon/helpers/class-bind.js
@@ -2,7 +2,7 @@ import { helper } from "@ember/component/helper";
 
 function classBind([classNameBindings, context]) {
   const bindings = classNameBindings.split(' ');
-  bindings.map((b) => {
+  return bindings.map((b) => {
     const [key, className] = b.split(':');
     return context[key] ? className : '';
   }).join(' ');
