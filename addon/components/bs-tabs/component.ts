daab6467150d27c53b390f7d4a9fe04d8eec50fb
patrick <pa.trickjlp@gmail.com>
2023-02-24 15:14:19 +0100

    fix tabs

--- addon/components/bs-tabs/component.ts
+++ addon/components/bs-tabs/component.ts
@@ -1,13 +1,25 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 type Args = {
-  onItemSelected(): void
+  onItemSelected(item: any): void
   contentPath: string
   selected: any
   content: any[]
 }
 
 // eslint-disable-next-line ember/no-empty-glimmer-component-classes
-export default class BsTabs extends Component<Args> {
+export default class BsTabsManager extends Component<Args> {
+  @tracked _selected = null;
 
+  get selected() {
+    return this.args.selected ?? this._selected ?? this.args.content?.[0];
+  }
+
+  @action
+  onItemSelected(item) {
+    this.args.onItemSelected?.(item);
+    this._selected = item;
+  }
 }
