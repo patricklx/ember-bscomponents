d16d7ad745cf4df00ad78343d70a58d0242ecf31
Patrick Pircher <patricklx@users.noreply.github.com>
2023-02-24 17:42:10 +0100

    Create component.ts

---
+++ addon/components/bs-tabs/pane/component.ts
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+
+type Args = {
+  id: any
+}
+
+// eslint-disable-next-line ember/no-empty-glimmer-component-classes
+export default class BsTabsPane extends Component<Args> {
+}
